8561da2a1dee4b95733c41b045a636db0bbbd20f
Ercan OLCAY <3rcanolcay@gmail.com>
2026-07-04 18:49:38 +0300

    Refactor main.c for improved UART handling and comments
    Updated the header comments and modified the handling of UART data reception. Adjusted the configuration parameters for the device.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,7 +2,7 @@
 /*
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Ercan OLCAY # BlueTRace APRS Tracker
+  * @brief          : Ercan OLCAY # BlindTRack APRS Tracker
   * 23.04.2026
   ******************************************************************************
   */
@@ -43,9 +43,9 @@ UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
 volatile uint8_t g_rxByte;        // GPS bytes (interrupt)
-volatile uint8_t rx_byte3;         // Config UART byte (interrupt)
-char             rx_buf3[128];     // Config UART buffer
-volatile uint8_t rx_idx3 = 0;     // Config UART buffer index (interrupt)
+uint8_t          rx_byte3;        // Config UART RX byte
+char             rx_buf3[128];    // Config UART buffer
+volatile uint8_t rx_idx3 = 0;    // Config UART buffer index (interrupt)
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,7 +56,7 @@ static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
-uint8_t Ax25IsSending(void); //External Function for Hardware PTT Control
+uint8_t Ax25IsSending(void); // External Function for Hardware PTT Control
 void WdogReset(void);
 /* USER CODE END PFP */
 
@@ -65,88 +65,63 @@ void WdogReset(void);
 
 /* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
-  /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM4_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
-  Load_Config_From_Flash(); // Check the memory and wake up!
+  Load_Config_From_Flash();
   MX_USART3_UART_Init();
   HAL_UART_Receive_IT(&huart3, &rx_byte3, 1);
+
   /* USER CODE BEGIN 2 */
-      // 72 MHz Interrupt Priority
-      HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
-      HAL_NVIC_EnableIRQ(TIM4_IRQn);
+  HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(TIM4_IRQn);
 
-      ModemInit();
-      Ax25Init();
+  ModemInit();
+  Ax25Init();
 
-      HAL_UART_Receive_IT(&huart1, &g_rxByte, 1);
+  HAL_UART_Receive_IT(&huart1, &g_rxByte, 1);
 
-      // PWM and Interrupt Start
-      HAL_TIM_Base_Start_IT(&htim4);
-      HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
-      // Flag sending aprs after reset
-      //static uint8_t is_restart_tx_done = 0;
-      extern uint32_t last_tx;
-      last_tx = 0xFFF00000;
+  HAL_TIM_Base_Start_IT(&htim4);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+
+  extern uint32_t last_tx;
+  last_tx = 0xFFF00000;
   /* USER CODE END 2 */
 
-  /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-      while (1)
-          {
-
-              Tracker_Run();
-              // Millis safety
-              if(HAL_GetTick() > 0xFFFFF000)
-                  NVIC_SystemReset();
+  while (1)
+  {
+      Tracker_Run();
+      // Millis overflow safety
+      if (HAL_GetTick() > 0xFFFFF000)
+          NVIC_SystemReset();
     /* USER CODE END WHILE */
-
     /* USER CODE BEGIN 3 */
-    }
+  }
   /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -154,90 +129,45 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) Error_Handler();
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) Error_Handler();
 }
 
-/**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_TIM4_Init(void)
 {
-
-  /* USER CODE BEGIN TIM4_Init 0 */
-
-  /* USER CODE END TIM4_Init 0 */
-
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
 
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 1499;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK) Error_Handler();
+
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK) Error_Handler();
+
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM4_Init 2 */
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) Error_Handler();
 
-  /* USER CODE END TIM4_Init 2 */
   HAL_TIM_MspPostInit(&htim4);
-
 }
 
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_USART1_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -246,31 +176,11 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.Mode = UART_MODE_TX_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
+  if (HAL_UART_Init(&huart1) != HAL_OK) Error_Handler();
 }
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_USART2_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -279,31 +189,11 @@ static void MX_USART2_UART_Init(void)
   huart2.Init.Mode = UART_MODE_TX_RX;
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
+  if (HAL_UART_Init(&huart2) != HAL_OK) Error_Handler();
 }
 
-/**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_USART3_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
   huart3.Instance = USART3;
   huart3.Init.BaudRate = 9600;
   huart3.Init.WordLength = UART_WORDLENGTH_8B;
@@ -312,57 +202,32 @@ static void MX_USART3_UART_Init(void)
   huart3.Init.Mode = UART_MODE_TX_RX;
   huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
+  if (HAL_UART_Init(&huart3) != HAL_OK) Error_Handler();
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(PTT_SIGNAL_LED_GPIO_Port, PTT_SIGNAL_LED_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PTT_SIGNAL_LED_Pin */
   GPIO_InitStruct.Pin = PTT_SIGNAL_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(PTT_SIGNAL_LED_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PA1 */
   GPIO_InitStruct.Pin = GPIO_PIN_1;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -373,74 +238,64 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
         HAL_UART_Receive_IT(&huart1, &g_rxByte, 1);
     }
 
-    // 2. Settings from Python Interface (USART3)
+    // 2. Settings from Configurator (USART3)
     if (huart->Instance == USART3) {
-            if (rx_byte3 == '\n' || rx_byte3 == '\r') {
-                rx_buf3[rx_idx3] = '\0';
+        if (rx_byte3 == '\n' || rx_byte3 == '\r') {
+            rx_buf3[rx_idx3] = '\0';
 
-                // 1. "WRITE" Request From Python
-                if (strncmp(rx_buf3, "CFG:", 4) == 0) {
-                    Parse_Config(rx_buf3 + 4);
-                }
-                // 2. "READ" Request From Python
-                else if (strncmp(rx_buf3, "GET_CFG", 7) == 0) {
-                    char tx_buf[128];
-                    sprintf(tx_buf, "CFG:%s,%d,%d,%c%c,%s\n",
-                            device_config.callsign,
-                            device_config.ssid,
-                            device_config.interval,
-                            device_config.table_id,
-                            device_config.symbol,
-                            device_config.comment);
-
-                    // Send data to Phyton
-                    HAL_UART_Transmit(&huart3, (uint8_t*)tx_buf, strlen(tx_buf), 500);
-                }
-
-                rx_idx3 = 0;
-            } else {
-                if (rx_idx3 < sizeof(rx_buf3) - 1) {
-                    rx_buf3[rx_idx3++] = rx_byte3;
-                } else {
-                    rx_idx3 = 0;
-                }
+            // "WRITE" request from configurator
+            if (strncmp(rx_buf3, "CFG:", 4) == 0) {
+                Parse_Config(rx_buf3 + 4);
             }
-            HAL_UART_Receive_IT(&huart3, &rx_byte3, 1);
+            // "READ" request from configurator
+            else if (strncmp(rx_buf3, "GET_CFG", 7) == 0) {
+                char tx_buf[192];
+                sprintf(tx_buf, "CFG:%s,%d,%d,%c%c,%d,%d,%d,%d,%d,%d,%d,%s\n",
+                        device_config.callsign,
+                        device_config.ssid,
+                        device_config.interval,
+                        device_config.table_id,
+                        device_config.symbol,
+                        device_config.smart_beacon,
+                        device_config.sb_low_speed,
+                        device_config.sb_high_speed,
+                        device_config.sb_slow_rate,
+                        device_config.sb_fast_rate,
+                        device_config.sb_turn_min,
+                        device_config.sb_turn_slope,
+                        device_config.comment);
+                HAL_UART_Transmit(&huart3, (uint8_t*)tx_buf, strlen(tx_buf), 500);
+            }
+
+            rx_idx3 = 0;
+        } else {
+            if (rx_idx3 < sizeof(rx_buf3) - 1) {
+                rx_buf3[rx_idx3++] = rx_byte3;
+            } else {
+                rx_idx3 = 0;
+            }
         }
+        HAL_UART_Receive_IT(&huart3, &rx_byte3, 1);
+    }
 }
 
 void WdogReset(void) {
-    /* IWDG kullanılmıyor — gerekirse HAL_IWDG_Refresh(&hiwdg) ekle */
+    /* IWDG not used — add HAL_IWDG_Refresh(&hiwdg) here if needed */
 }
 /* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
+  while (1) {}
   /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
